5c6a9eefc8af4e48a7301b8f82fde1bd0230391d
zchakl <zacharyhakl@gmail.com>
2022-09-03 12:49:35 -0500

    changed some things in toggleterm setup

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -13,7 +13,7 @@ toggleterm.setup({
 	start_in_insert = true,
 	insert_mappings = true,
 	persist_size = true,
-	direction = "float",
+	direction = "horizontal",
 	close_on_exit = true,
 	shell = "/bin/zsh",
 	float_opts = {
@@ -39,31 +39,31 @@ end
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
 
 function _LAZYGIT_TOGGLE()
 	lazygit:toggle()
 end
 
-local node = Terminal:new({ cmd = "node", hidden = true })
+local node = Terminal:new({ cmd = "node", hidden = true, direction = "float" })
 
 function _NODE_TOGGLE()
 	node:toggle()
 end
 
-local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
+local ncdu = Terminal:new({ cmd = "ncdu", hidden = true, direction = "float" })
 
 function _NCDU_TOGGLE()
 	ncdu:toggle()
 end
 
-local htop = Terminal:new({ cmd = "htop", hidden = true })
+local htop = Terminal:new({ cmd = "htop", hidden = true, direction = "float"  })
 
 function _HTOP_TOGGLE()
 	htop:toggle()
 end
 
-local python = Terminal:new({ cmd = "python", hidden = true })
+local python = Terminal:new({ cmd = "python", hidden = true, direction = "float"  })
 
 function _PYTHON_TOGGLE()
 	python:toggle()
